aa023b97e9b84b445c32bdac083f960f8a11fb13
Piyush5767 <piyush.sharma5767@gmail.com>
2017-04-09 23:59:08 -0400

    Uploading Folder Files
    Uploading new files and folder

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Babystore.Startup))]
+namespace Babystore
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
